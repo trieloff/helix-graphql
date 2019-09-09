8388362e92b27c70281d0d9d665b6fab459596a2
Lars Trieloff <trieloff@adobe.com>
2019-09-09 13:48:53 +0000

    feat(contents): enable filtering contents with `match` expression

--- src/resolvers/index.mjs
+++ src/resolvers/index.mjs
@@ -1,16 +1,19 @@
+import fetch from "node-fetch";
+import minimatch from "minimatch";
 
-
-async function repo({org, name}) {
+async function repo({owner, repo}) {
   return {
-    org,
-    name,
-    contents: ({ref}) => {
-      console.log(ref);
-      return [
-        {
-          path: ref + 'README.md',
-        }
-      ]
+    owner,
+    name: repo,
+    contents: ({ref = 'master', path = '', match = '*'}) => {
+      try {
+        return fetch(`https://api.github.com/repos/${owner}/${repo}/contents/${path}?ref=${ref}`)
+          .then(res => res.json())
+          .then(res => res.filter(({name}) => minimatch(name, match)));
+      } catch (e) {
+        console.error(e);
+        return [];
+      }
     }
   }
 }
