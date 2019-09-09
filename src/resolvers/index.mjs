ba408fbf12fe2d6dff7df28eef8ab2ee6230c2ce
Lars Trieloff <trieloff@adobe.com>
2019-09-09 14:21:35 +0000

    feat(document): implement document metadata fetching

--- src/resolvers/index.mjs
+++ src/resolvers/index.mjs
@@ -1,6 +1,18 @@
 import fetch from "node-fetch";
 import minimatch from "minimatch";
 
+function document(owner, repo, ref, name) {
+  return async (e) => {
+    console.log('fetching metadata', e, `https://adobeioruntime.net/api/v1/web/helix-pages/dynamic%40v1/idx_json?owner=${owner}&repo=${repo}&ref=${ref}&path=${name}`);
+    return fetch(`https://adobeioruntime.net/api/v1/web/helix-pages/dynamic%40v1/idx_json?owner=${owner}&repo=${repo}&ref=${ref}&path=${name}`)
+      .then(res => res.json())
+      .then(res => ({
+        ...res.basic.entries,
+        ...res.images.entries
+      }));
+  }
+}
+
 async function repo({owner, repo}) {
   return {
     owner,
@@ -9,7 +21,11 @@ async function repo({owner, repo}) {
       try {
         return fetch(`https://api.github.com/repos/${owner}/${repo}/contents/${path}?ref=${ref}`)
           .then(res => res.json())
-          .then(res => res.filter(({name}) => minimatch(name, match)));
+          .then(res => res.filter(({name}) => minimatch(name, match)))
+          .then(res => res.map(content => {
+            content.document = document(owner, repo, ref, content.name);
+            return content;
+          }));
       } catch (e) {
         console.error(e);
         return [];
