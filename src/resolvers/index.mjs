a28c5d3ddb746c4f92b5833b45280237c7f43d47
Lars Trieloff <trieloff@adobe.com>
2019-09-13 11:24:17 +0000

    feat(resolver): enable recursive fetching of files

--- src/resolvers/index.mjs
+++ src/resolvers/index.mjs
@@ -13,17 +13,35 @@ function document(owner, repo, ref, name) {
   }
 }
 
+async function fetchdir(url, depth = 0) {
+  const contents = await fetch(url).then(res => res.json());
+  console.log(url, depth, contents);
+  const folders = contents.filter(({ type}) => type === 'dir');
+  const files = contents.filter(({ type}) => type === 'file');
+
+  if (depth > 0 && folders.length > 0) {
+    const jobs = folders.map(({_links}) => fetchdir(_links.self, depth - 1));
+    const subfiles = await Promise.all(jobs);
+    return subfiles.reduce((p, v) => {
+      return [...p, ...v];
+    }, files);
+  }
+  
+  return files;
+}
+
 async function repo({owner, repo}) {
   return {
     owner,
     name: repo,
-    contents: ({ref = 'master', path = '', match = '*'}) => {
+    contents: ({ref = 'master', path = '', match = '*', depth = 0}) => {
       try {
-        return fetch(`https://api.github.com/repos/${owner}/${repo}/contents/${path}?ref=${ref}`)
-          .then(res => res.json())
+
+        return fetchdir(`https://api.github.com/repos/${owner}/${repo}/contents/${path}?ref=${ref}`, depth)
+          //.then(res => res.json())
           .then(res => res.filter(({name}) => minimatch(name, match)))
           .then(res => res.map(content => {
-            content.document = document(owner, repo, ref, content.name);
+            content.document = document(owner, repo, ref, content.path);
             return content;
           }));
       } catch (e) {
@@ -35,5 +53,5 @@ async function repo({owner, repo}) {
 }
 
 export default {
-  repo
+  repo, fetchdir
 }
